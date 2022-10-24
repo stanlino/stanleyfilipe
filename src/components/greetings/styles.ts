af459d800963d518bc05e742b2de4a7efca3b3ed
Stanley Filipe <stanley92filipe@gmail.com>
2022-10-24 08:52:43 -0300

    feat: greetings component

--- src/components/greetings/styles.ts
+++ src/components/greetings/styles.ts
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Text = styled.h1`
   margin: 0;
-  line-height: 1.15;
-  font-size: 1.75rem;
+  line-height: 2rem;
+  font-size: 1.60rem;
   text-align: center;
   width: 100%;
 
@@ -17,5 +17,6 @@ export const Text = styled.h1`
 
   @media (min-width: 720px) {
     font-size: 3rem;
+    line-height: 4rem;
   }
 `

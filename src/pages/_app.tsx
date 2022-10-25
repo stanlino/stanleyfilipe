792b28388867204f485b697572d31af5ae651fd9
Stanley Filipe <stanley92filipe@gmail.com>
2022-10-24 22:20:54 -0300

    feat: nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
 import type { AppProps } from 'next/app'
+import nProgress from 'nprogress'
+import { Router } from 'next/router'
+
+import "../styles/nprogress.css"
 import { GlobalStyle } from '../styles/global'
 
 import '../lang'
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
+
+nProgress.configure({
+  showSpinner: false,
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (

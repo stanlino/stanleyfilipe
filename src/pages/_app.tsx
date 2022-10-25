fcd118586f97b89709e206d95fe900d2387d4860
Stanley Filipe <stanley92filipe@gmail.com>
2022-10-24 22:27:35 -0300

    refactor: nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,11 @@
 import type { AppProps } from 'next/app'
-import nProgress from 'nprogress'
-import { Router } from 'next/router'
 
-import "../styles/nprogress.css"
 import { GlobalStyle } from '../styles/global'
 
 import '../lang'
 
-Router.events.on("routeChangeStart", nProgress.start);
-Router.events.on("routeChangeError", nProgress.done);
-Router.events.on("routeChangeComplete", nProgress.done);
-
-nProgress.configure({
-  showSpinner: false,
-})
+import "../styles/nprogress.css"
+import '../components/nprogress'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (

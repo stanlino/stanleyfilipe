769a178b6dd4dbbed0bb17db9f5ea508f5609d15
Stanley Filipe <stanley92filipe@gmail.com>
2022-12-12 12:38:07 -0300

    feat: add project

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,15 +1,23 @@
 const projects = [
   {
+    name: 'RN Calculator',
+    description: 'A simple calculator app',
+    technologies: ['React Native', 'Expo', 'Styled-Components'],
+    github: 'https://github.com/stanlino/rncalculator',
+    demo: 'https://github.com/stanlino/rncalculator',
+    image: '/projects/rn_calculator.png',
+  },
+  {
     name: 'Archaic chat',
     description: 'A simple chat app',
     technologies: ['React', 'Tailwind', 'Node.js', 'Express', 'Socket.io'],
     github: 'https://github.com/stanlino/archaic_chat_web',
-    demo: 'https://archaic.netlify.app',
+    demo: 'https://github.com/stanlino/archaic_chat_web',
     image: '/projects/archaic_chat_web.png',
   },
   {
     name: 'Archaic chat Mobile',
-    description: 'A simple chat app for mobile',
+    description: 'A simple mobile chat app',
     technologies: ['React Native', 'Expo', 'Styled-Components', 'Socket.io'],
     github: 'https://github.com/stanlino/archaic_chat_mobile',
     demo: 'https://github.com/stanlino/archaic_chat_mobile',
